87a975b633c427b8125fa85f5ad7d5968b52022e
Johan Lund <drancisdrake@gmail.com>
2020-09-09 18:29:30 +0200

    init commit

---
+++ app/src/main/java/com/example/mathgame/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.mathgame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView question = findViewById(R.id.mathQuestion);
+        question.setText("3 + 6 =");
+    }
+}
