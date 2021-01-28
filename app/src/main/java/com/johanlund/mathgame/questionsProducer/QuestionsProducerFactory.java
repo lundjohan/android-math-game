365c0e46742a88d664cd8d03f762611a9550065c
Johan Lund <drancisdrake@gmail.com>
2021-01-28 14:09:19 +0100

    change necessary for test classes

--- app/src/main/java/com/johanlund/mathgame/questionsProducer/QuestionsProducerFactory.java
+++ app/src/main/java/com/johanlund/mathgame/questionsProducer/QuestionsProducerFactory.java
@@ -4,5 +4,5 @@ import dagger.Component;
 
 @Component(modules = QuestionsProducerModule.class)
 public interface QuestionsProducerFactory {
-    QuestionsProducerImpl questionsProducer();
+    QuestionsProducer questionsProducer();
 }
